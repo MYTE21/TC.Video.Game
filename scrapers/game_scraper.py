0dc95c4368a11d7b4acbcb405b0f2c275d490942
MYTE <yasmi.tohabar@gmail.com>
2023-08-12 00:34:53 +0600

    get link game done

--- scrapers/game_scraper.py
+++ scrapers/game_scraper.py
@@ -35,6 +35,19 @@ def get_all_game():
     driver.close()
 
 
+def get_game_link():
+    page = 0
+    driver = webdriver.Chrome()
+    url = f"https://www.metacritic.com/browse/games/score/metascore/all/all/?page={page}"
+    driver.get(url)
+
+    browser_list = driver.find_elements(By.CLASS_NAME, "browse_list_wrapper")[0]
+    table_list = browser_list.find_element(By.XPATH, "//tr").find_elements(By.XPATH, "//td")[0].find_element(By.TAG_NAME, "a").get_attribute("href")
+    print(table_list)
+
+    driver.quit()
+
+
 def read_page_no():
     with open("page_pointer.json", "r") as file:
         data = json.load(file)
@@ -49,4 +62,4 @@ def write_page_no(start, end):
 
 
 if __name__ == "__main__":
-    get_all_game()
+    get_game_link()
