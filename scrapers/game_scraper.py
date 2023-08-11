07f2f7c16317bb533e8cd7c600fcec4ee7e61067
MYTE <yasmi.tohabar@gmail.com>
2023-08-12 02:29:58 +0600

    game collection process ready

--- scrapers/game_scraper.py
+++ scrapers/game_scraper.py
@@ -2,8 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pandas as pd
 import json
+from tqdm import tqdm
 
-columns = []
+columns = ["Name", "Summary", "Link"]
 
 
 def game_details():
@@ -12,40 +13,33 @@ def game_details():
 
 def get_all_game():
     game_data = []
+    total_games = 0
     driver = webdriver.Chrome()
 
     start_page, end_page = read_page_no()
 
-    for page_id in range(start_page, end_page):
+    for page_id in tqdm(range(start_page, end_page)):
+        print(f"\nPage No: {page_id + 1}")
         url = f"https://www.metacritic.com/browse/games/score/metascore/all/all/?page={page_id}"
         driver.get(url)
 
         browser_list = driver.find_elements(By.CLASS_NAME, "browse_list_wrapper")
-        print("Browser: ", len(browser_list))
+        # print("Browser: ", len(browser_list))
         for browser in browser_list:
-            anime_table = browser.find_element(By.CLASS_NAME, "clamp-list")
-            anime_row = anime_table.find_elements(By.TAG_NAME, "tr")
-            for i, ar in enumerate(anime_row):
+            game_table = browser.find_element(By.CLASS_NAME, "clamp-list")
+            game_row = game_table.find_elements(By.TAG_NAME, "tr")
+            for i, ar in enumerate(game_row):
                 if i % 2 == 0:
-                    anime_details = ar.find_elements(By.TAG_NAME, "td")[1].text.split("\n")
-                    print(f"({i}): {anime_details}")
+                    table_list = browser.find_element(By.XPATH, "//tr").find_elements(By.XPATH, "//td")[i]
+                    link = table_list.find_element(By.TAG_NAME, "a").get_attribute("href")
+                    game_detail = ar.find_elements(By.TAG_NAME, "td")[1].text.split("\n")
+                    total_games += 1
+                    # print(f"({i}): {game_detail} \n link: {link}")
 
-        break
+        write_page_no(start_page + 1, end_page)
+        print("Total games: ", total_games)
 
     driver.close()
-
-
-def get_game_link():
-    page = 0
-    driver = webdriver.Chrome()
-    url = f"https://www.metacritic.com/browse/games/score/metascore/all/all/?page={page}"
-    driver.get(url)
-
-    browser_list = driver.find_elements(By.CLASS_NAME, "browse_list_wrapper")[0]
-    table_list = browser_list.find_element(By.XPATH, "//tr").find_elements(By.XPATH, "//td")[0].find_element(By.TAG_NAME, "a").get_attribute("href")
-    print(table_list)
-
-    driver.quit()
 
 
 def read_page_no():
@@ -62,4 +56,4 @@ def write_page_no(start, end):
 
 
 if __name__ == "__main__":
-    get_game_link()
+    get_all_game()
